278ebacad18c1cfa256b0fe2f166340b4d87484b
Tom Hacohen <tom@stosb.com>
2017-12-07 00:08:23 +0000

    Add a nicer loading indicator.

---
+++ src/LoadingIndicator.tsx
@@ -0,0 +1,8 @@
+import * as React from 'react';
+import CircularProgress from 'material-ui/CircularProgress';
+
+export default (props: any) => {
+  return (
+    <CircularProgress size={60} thickness={7} />
+  );
+};
