34445be4b01cc2c9b08eee37a92326c4eafb2a52
Tom Hacohen <tom@stosb.com>
2017-12-08 17:58:24 +0000

    Improve container

--- src/Container.tsx
+++ src/Container.tsx
@@ -1,7 +1,14 @@
 import * as React from 'react';
+import Paper from 'material-ui/Paper';
 
 import './Container.css';
 
 export default ({children}: {children: any}) => (
-  <div className="Container">{children}</div>
+  <div className="Container">
+    <Paper zDepth={2}>
+      <div className="Container-inner">
+        {children}
+      </div>
+    </Paper>
+  </div>
 );
