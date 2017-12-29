2de13c9c0febafb4265a571e7e098507c8d89d43
Tom Hacohen <tom@stosb.com>
2017-12-29 14:43:52 +0000

    Add a deployment script that supports signed pages.

---
+++ deploy.sh
@@ -0,0 +1,12 @@
+SSH_HOST=client.etesync.com
+SSH_PORT=22
+SSH_USER=etesync
+SSH_TARGET_DIR=sites/client.etesync.com
+
+OUTPUTDIR=./build
+
+yarn build
+
+sed -i "s#\(<script type=\"text/javascript\"\)#\1 integrity=\"sha384-$(shasum -b -a 384 build/static/js/main.*.js | xxd -r -p | base64 -w0)\" crossorigin=\"anonymous\"#" build/index.html
+./page-signer.js build/index.html build/index.html
+rsync -e "ssh -p ${SSH_PORT}" -P --delete -rvzc --exclude '*.map' ${OUTPUTDIR}/ ${SSH_USER}@${SSH_HOST}:${SSH_TARGET_DIR}
