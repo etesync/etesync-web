6d0614df635ea4a61a4de0d0d9ae4f10813a7e4f
Tom Hacohen <tom@stosb.com>
2018-10-24 23:03:10 +0100

    Container: upgrade material-ui.

--- src/widgets/Container.tsx
+++ src/widgets/Container.tsx
@@ -1,11 +1,11 @@
 import * as React from 'react';
-import Paper from 'material-ui/Paper';
+import Paper from '@material-ui/core/Paper';
 
 import './Container.css';
 
 export default (props: {style?: any, children: any}) => (
   <div className="Container" style={props.style}>
-    <Paper zDepth={2}>
+    <Paper elevation={3}>
       <div className="Container-inner">
         {props.children}
       </div>
