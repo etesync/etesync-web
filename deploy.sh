3d52fc26345cfead57287984224a666407f7593c
Tom Hacohen <tom@stosb.com>
2020-02-05 13:02:07 +0200

    Deploy script: change rsync invocation.
    It was failing with latest arch.

--- deploy.sh
+++ deploy.sh
@@ -11,4 +11,4 @@ yarn build
 
 sed -i "s#\(<script type=\"text/javascript\"\)#\1 integrity=\"sha384-$(shasum -b -a 384 build/static/js/main.*.js | xxd -r -p | base64 -w0)\" crossorigin=\"anonymous\"#" build/index.html
 ./page-signer.js build/index.html build/index.html
-rsync -e "ssh -p ${SSH_PORT}" -P --delete -rvzc ${OUTPUTDIR}/ ${SSH_USER}@${SSH_HOST}:${SSH_TARGET_DIR}
+rsync -e "ssh -p ${SSH_PORT}" -P --delete -rvc -zz ${OUTPUTDIR}/ ${SSH_USER}@${SSH_HOST}:${SSH_TARGET_DIR}
