0436e9ccd02cd775a9f847a8e2b2047d90e5a827
Andrew P Maney <amaney@usc.edu>
2020-03-12 15:04:17 -0700

    Tasks: adds colored radio buttons
    available as widget, they are being used for priority in Task Edit

---
+++ src/widgets/ColoredRadio.tsx
@@ -0,0 +1,29 @@
+import * as React from 'react';
+
+import { makeStyles } from '@material-ui/core/styles';
+import Radio from '@material-ui/core/Radio';
+import { Omit } from '@material-ui/types';
+import FormControlLabel, { FormControlLabelProps } from '@material-ui/core/FormControlLabel';
+
+interface Props {
+  color: string;
+  label: string;
+}
+
+const useStyles = makeStyles({
+  root: {
+    color: (props: Props) => props.color,
+  },
+});
+
+export default function ColoredRadio(props: Props & Omit<FormControlLabelProps, keyof Props | 'control'>) {
+  const { color, label, value, ...other } = props;
+  const { root } = useStyles(props);
+
+  return <FormControlLabel
+    className={root}
+    label={label}
+    control={<Radio color="default" className={root} value={value} />}
+    {...other}
+  />;
+}
