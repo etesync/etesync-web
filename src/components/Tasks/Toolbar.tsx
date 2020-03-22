bdd9bc67a0179673c6040e402ed65d1e1692abc7
Andrew P Maney <amaney@usc.edu>
2020-03-22 02:33:30 -0700

    Tasks: redesigns sort menu to simple icon

--- src/components/Tasks/Toolbar.tsx
+++ src/components/Tasks/Toolbar.tsx
@@ -8,9 +8,7 @@ import IconButton from '@material-ui/core/IconButton';
 import MoreVertIcon from '@material-ui/icons/MoreVert';
 import Menu from '@material-ui/core/Menu';
 import MenuItem from '@material-ui/core/MenuItem';
-import Select from '@material-ui/core/Select';
-import FormControl from '@material-ui/core/FormControl';
-import InputLabel from '@material-ui/core/InputLabel';
+import SortIcon from '@material-ui/icons/Sort';
 
 import QuickAdd from './QuickAdd';
 
@@ -31,37 +29,47 @@ interface PropsType {
 export default function Toolbar(props: PropsType) {
   const { defaultCollection, onItemSave, showCompleted, setShowCompleted } = props;
 
-  const [anchorEl, setAnchorEl] = React.useState<null | HTMLElement>(null);
+  const [sortAnchorEl, setSortAnchorEl] = React.useState<null | HTMLElement>(null);
+  const [optionsAnchorEl, setOptionsAnchorEl] = React.useState<null | HTMLElement>(null);
 
   const dispatch = useDispatch();
   const taskSettings = useSelector((state: StoreState) => state.settings.taskSettings);
+  const { sortBy } = taskSettings;
 
-  const handleSelectChange = (e: React.ChangeEvent<{ value: string }>) => {
-    dispatch(setSettings({ taskSettings: { ...taskSettings, sortBy: e.target.value } }));
+  const handleSortChange = (sort: string) => {
+    dispatch(setSettings({ taskSettings: { ...taskSettings, sortBy: sort } }));
   };
 
-  const handleClick = (event: React.MouseEvent<HTMLElement>) => {
-    setAnchorEl(event.currentTarget);
-  };
-
-  const handleClose = () => {
-    setAnchorEl(null);
-  };
+  const SortMenuItem = (props: { name: string, label: string }) => (
+    <MenuItem selected={sortBy === props.name} onClick={() => handleSortChange(props.name)}>{props.label}</MenuItem>
+  );
 
   return (
     <div style={{ display: 'flex', justifyContent: 'space-between' }}>
-      {defaultCollection && <QuickAdd style={{ flexGrow: 1, marginRight: '1.75em' }} onSubmit={onItemSave} defaultCollection={defaultCollection} />}
+      {defaultCollection && <QuickAdd style={{ flexGrow: 1, marginRight: '0.75em' }} onSubmit={onItemSave} defaultCollection={defaultCollection} />}
 
-      <FormControl style={{ marginRight: '0.75em' }}>
-        <InputLabel>Sort by</InputLabel>
-        <Select value={taskSettings.sortBy} onChange={handleSelectChange}>
-          <MenuItem value="smart">Smart</MenuItem>
-          <MenuItem value="dueDate">Due Date</MenuItem>
-          <MenuItem value="priority">Priority</MenuItem>
-          <MenuItem value="title">Title</MenuItem>
-          <MenuItem value="lastModifiedDate">Last Modified</MenuItem>
-        </Select>
-      </FormControl>
+      <div>
+        <IconButton
+          aria-label="more"
+          aria-controls="long-menu"
+          aria-haspopup="true"
+          onClick={(e) => setSortAnchorEl(e.currentTarget)}
+        >
+          <SortIcon />
+        </IconButton>
+        <Menu
+          anchorEl={sortAnchorEl}
+          keepMounted
+          open={!!sortAnchorEl}
+          onClose={() => setSortAnchorEl(null)}
+        >
+          <SortMenuItem name="smart" label="Smart" />
+          <SortMenuItem name="dueDate" label="Due Date" />
+          <SortMenuItem name="priority" label="Priority" />
+          <SortMenuItem name="title" label="Title" />
+          <SortMenuItem name="lastModifiedDate" label="Last Modified" />
+        </Menu>
+      </div>
 
 
       <div>
@@ -69,16 +77,16 @@ export default function Toolbar(props: PropsType) {
           aria-label="more"
           aria-controls="long-menu"
           aria-haspopup="true"
-          onClick={handleClick}
+          onClick={(e) => setOptionsAnchorEl(e.currentTarget)}
         >
           <MoreVertIcon />
         </IconButton>
         <Menu
           id="simple-menu"
-          anchorEl={anchorEl}
+          anchorEl={optionsAnchorEl}
           keepMounted
-          open={!!anchorEl}
-          onClose={handleClose}
+          open={!!optionsAnchorEl}
+          onClose={() => setOptionsAnchorEl(null)}
         >
           <MenuItem>
             <FormControlLabel
