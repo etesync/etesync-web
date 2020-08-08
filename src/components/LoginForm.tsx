58533f165dc8115aa136e623ebfe83924a460679
Tom Hacohen <tom@stosb.com>
2020-08-08 10:04:14 +0300

    Login form: call the username username rather than email.

--- src/components/LoginForm.tsx
+++ src/components/LoginForm.tsx
@@ -143,11 +143,11 @@ class LoginForm extends React.PureComponent {
         {(this.props.error) && (<div>Error! {this.props.error.message}</div>)}
         <form style={styles.form} onSubmit={this.generateEncryption}>
           <TextField
-            type={this.state.showAdvanced ? "text" : "email"}
+            type="text"
             style={styles.textField}
             error={!!this.state.errors.errorEmail}
             helperText={this.state.errors.errorEmail}
-            label="Email"
+            label="Username"
             name="username"
             value={this.state.username}
             onChange={this.handleInputChange}
