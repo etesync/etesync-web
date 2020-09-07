0c59aef1dc45427f9c5bc7bc88dde0abdd9d0c2d
Tom Hacohen <tom@stosb.com>
2020-09-07 14:20:02 +0300

    Forgot password: fix the link to point to the faq entry.

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -8,4 +8,4 @@ export const faq = homePage + "faq/";
 export const sourceCode = "https://github.com/etesync/etesync-web";
 export const reportIssue = sourceCode + "/issues";
 
-export const forgotPassword = "https://www.etesync.com/accounts/password/reset/";
+export const forgotPassword = "https://www.etesync.com/faq/#forgot-password";
