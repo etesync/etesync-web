d530204584f5b46f9a98f6c2c1ad2eb75046f746
Tom Hacohen <tom@stosb.com>
2020-10-15 18:18:02 +0300

    pim-types: Fixed group detection.

--- src/pim-types.ts
+++ src/pim-types.ts
@@ -387,6 +387,6 @@ export class ContactType implements PimType {
 
   get group() {
     const kind = this.comp.getFirstPropertyValue("kind");
-    return kind in ["group", "organization"];
+    return ["group", "organization"].includes(kind);
   }
 }
