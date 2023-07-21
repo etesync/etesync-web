98ddff8f52522e53f0bdc091dfd03eba6c55e447
MangoCubes <10383115+MangoCubes@users.noreply.github.com>
2023-07-21 16:37:24 +0900

    Tasks edit: Added toggle switch for hidden and completed tasks

--- src/Tasks/TaskSelectorListItem.tsx
+++ src/Tasks/TaskSelectorListItem.tsx
@@ -4,12 +4,15 @@ import React from "react";
 
 interface PropsType {
   entries: TaskType[];
+  showHidden: boolean;
+  showCompleted: boolean;
   onClick: (uid: string) => void;
   thisEntry: TaskType;
 }
 
 export default function TaskSelectorListItem(props: PropsType) {
-  const tasks = props.entries.filter((e) => e.relatedTo === props.thisEntry.uid);
+  const tasks = props.entries
+    .filter((e) => e.relatedTo === props.thisEntry.uid && (props.showHidden || !e.hidden) && (props.showCompleted || !e.finished));
 
   return (
     <ListItem
@@ -18,6 +21,8 @@ export default function TaskSelectorListItem(props: PropsType) {
       onClick={() => props.onClick(props.thisEntry.uid)}
       nestedItems={tasks.map((e) => 
         <TaskSelectorListItem
+          showHidden={props.showHidden}
+          showCompleted={props.showCompleted}
           key={e.uid}
           entries={props.entries}
           onClick={props.onClick}
